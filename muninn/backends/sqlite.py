stcorp/muninn

commit bd32f94e6a5bc3603a946760ab8c7f14e56b1675

Close SQL connection after ingestion/search/etc

muninn/backends/sqlite.py
@@ -172,6 +172,7 @@ class SQLiteConnection(object):
             self._connection.rollback()
 
         self._in_transaction = False
+        self.close()
 
     def _connect(self):
         # Re-establish the connection to the database.

muninn/backends/postgresql.py
@@ -151,6 +151,7 @@ class PostgresqlConnection(object):
             self._connection.rollback()
 
         self._in_transaction = False
+        self.close()
 
     def _connect(self):
         # Re-establish the connection to the database.

muninn/tools/destroy.py
@@ -36,21 +36,21 @@ def ask_yes_no(question, default=True):
 
 
 def destroy(args):
-    archive = muninn.open(args.archive)
-    if not args.yes:
-        if args.catalogue_only:
-            print(("You are about to remove the catalogue database for the archive \"%s\". " +
-                   "This operation cannot be undone!") % args.archive)
-        else:
-            print(("You are about to completely remove the archive \"%s\". " +
-                   "This operation cannot be undone!") % args.archive)
-        if not ask_yes_no("Do you want to continue?", False):
-            return 1
+    with muninn.open(args.archive) as archive:
+        if not args.yes:
+            if args.catalogue_only:
+                print(("You are about to remove the catalogue database for the archive \"%s\". " +
+                       "This operation cannot be undone!") % args.archive)
+            else:
+                print(("You are about to completely remove the archive \"%s\". " +
+                       "This operation cannot be undone!") % args.archive)
+            if not ask_yes_no("Do you want to continue?", False):
+                return 1
 
-    if args.catalogue_only:
-        archive.destroy_catalogue()
-    else:
-        archive.destroy()
+        if args.catalogue_only:
+            archive.destroy_catalogue()
+        else:
+            archive.destroy()
     return 0
 
 

muninn/tools/export.py
@@ -21,22 +21,21 @@ def log_internal_error():
 
 
 def export(args):
-    archive = muninn.open(args.archive)
+    with muninn.open(args.archive) as archive:
+        if args.list_formats:
+            if not archive.export_formats():
+                print("no alternative export formats available")
+            else:
+                print("alternative export formats: " + " ".join(archive.export_formats()))
+            print("")
+            return 0
 
-    if args.list_formats:
-        if not archive.export_formats():
-            print("no alternative export formats available")
-        else:
-            print("alternative export formats: " + " ".join(archive.export_formats()))
-        print("")
-        return 0
+        if args.expression is None:
+            logging.error("no search expression specified")
+            return 1
 
-    if args.expression is None:
-        logging.error("no search expression specified")
-        return 1
-
-    target_path = os.getcwd() if args.directory is None else args.directory
-    archive.export(where=args.expression, target_path=target_path, format=args.format)
+        target_path = os.getcwd() if args.directory is None else args.directory
+        archive.export(where=args.expression, target_path=target_path, format=args.format)
     return 0
 
 

muninn/tools/ingest.py
@@ -68,48 +68,48 @@ def get_path_expansion_function(is_stem=False, is_enclosing_directory=False):
 
 
 def ingest(args):
-    archive = muninn.open(args.archive)
-    path_expansion_function = get_path_expansion_function(args.path_is_stem, args.path_is_enclosing_directory)
-    assert(not args.link or not args.copy)
-    use_symlinks = True if args.link else False if args.copy else None
-    verify_hash = True if args.verify_hash else False
+    with muninn.open(args.archive) as archive:
+        path_expansion_function = get_path_expansion_function(args.path_is_stem, args.path_is_enclosing_directory)
+        assert(not args.link or not args.copy)
+        use_symlinks = True if args.link else False if args.copy else None
+        verify_hash = True if args.verify_hash else False
 
-    errors_encountered = False
-    paths = sys.stdin if "-" in args.path else args.path
-    for path in paths:
-        path = os.path.abspath(path.strip())
+        errors_encountered = False
+        paths = sys.stdin if "-" in args.path else args.path
+        for path in paths:
+            path = os.path.abspath(path.strip())
 
-        # Expand path into multiple files and/or directories that belong to the same product.
-        try:
-            product_paths = path_expansion_function(path)
-        except Error as error:
-            logging.error("%s: unable to determine which files or directories belong to product [%s]" % (path, error))
-            errors_encountered = True
-            continue
-
-        # Discard paths matching any of the user supplied exclude patterns.
-        if args.exclude:
-            product_paths = filter_paths(product_paths, args.exclude)
-
-        if not product_paths:
-            logging.error("%s: path does not match any files or directories" % path)
-            errors_encountered = True
-            continue
-
-        try:
-            properties = archive.ingest(product_paths, args.product_type, use_symlinks=use_symlinks,
-                                        verify_hash=verify_hash)
-        except muninn.Error as error:
-            logging.error("%s: unable to ingest product [%s]" % (path, error))
-            errors_encountered = True
-            continue
-
-        if args.tag:
+            # Expand path into multiple files and/or directories that belong to the same product.
             try:
-                archive.tag(properties.core.uuid, args.tag)
-            except muninn.Error as error:
-                logging.error("%s: unable to tag product [%s]" % (path, error))
+                product_paths = path_expansion_function(path)
+            except Error as error:
+                logging.error("%s: unable to determine which files or directories belong to product [%s]" % (path, error))
                 errors_encountered = True
+                continue
+
+            # Discard paths matching any of the user supplied exclude patterns.
+            if args.exclude:
+                product_paths = filter_paths(product_paths, args.exclude)
+
+            if not product_paths:
+                logging.error("%s: path does not match any files or directories" % path)
+                errors_encountered = True
+                continue
+
+            try:
+                properties = archive.ingest(product_paths, args.product_type, use_symlinks=use_symlinks,
+                                            verify_hash=verify_hash)
+            except muninn.Error as error:
+                logging.error("%s: unable to ingest product [%s]" % (path, error))
+                errors_encountered = True
+                continue
+
+            if args.tag:
+                try:
+                    archive.tag(properties.core.uuid, args.tag)
+                except muninn.Error as error:
+                    logging.error("%s: unable to tag product [%s]" % (path, error))
+                    errors_encountered = True
 
     return 0 if not errors_encountered else 1
 

muninn/tools/list_tags.py
@@ -21,10 +21,10 @@ def log_internal_error():
 
 
 def list_tags(args):
-    archive = muninn.open(args.archive)
-    for product in archive.search(where=args.expression):
-        tags = archive.tags(product.core.uuid)
-        print("%s (%s): %s" % (product.core.product_name, product.core.uuid, ", ".join(tags)))
+    with muninn.open(args.archive) as archive:
+        for product in archive.search(where=args.expression):
+            tags = archive.tags(product.core.uuid)
+            print("%s (%s): %s" % (product.core.product_name, product.core.uuid, ", ".join(tags)))
 
     return 0
 

muninn/tools/prepare.py
@@ -20,15 +20,15 @@ def log_internal_error():
 
 
 def prepare(args):
-    archive = muninn.open(args.archive)
-    if args.dry_run:
-        print("The following SQL statements would be executed:")
-        for sql in archive.prepare_catalogue(dry_run=True):
-            print("  " + sql)
-    elif args.catalogue_only:
-        archive.prepare_catalogue()
-    else:
-        archive.prepare(force=args.force)
+    with muninn.open(args.archive) as archive:
+        if args.dry_run:
+            print("The following SQL statements would be executed:")
+            for sql in archive.prepare_catalogue(dry_run=True):
+                print("  " + sql)
+        elif args.catalogue_only:
+            archive.prepare_catalogue()
+        else:
+            archive.prepare(force=args.force)
     return 0
 
 

muninn/tools/pull.py
@@ -21,15 +21,15 @@ def log_internal_error():
 
 
 def pull(args):
-    archive = muninn.open(args.archive)
-    verify_hash = True if args.verify_hash else False
+    with muninn.open(args.archive) as archive:
+        verify_hash = True if args.verify_hash else False
 
-    # find all remote products that satisfy filter
-    expression = "(is_defined(remote_url) and not is_defined(archive_path))"
-    if args.expression:
-        expression = "%s and %s" % (expression, args.expression)
+        # find all remote products that satisfy filter
+        expression = "(is_defined(remote_url) and not is_defined(archive_path))"
+        if args.expression:
+            expression = "%s and %s" % (expression, args.expression)
 
-    num_products = archive.pull(expression, verify_hash=verify_hash)
+        num_products = archive.pull(expression, verify_hash=verify_hash)
 
     return 0
 

commit f1312963fb9ec574f46bbdd9976440ba9b53f912

Fixed (apparent) typo. This function broke after bd32f94e6a5bc3603a946760ab8c7f14e56b1675

muninn/backends/sqlite.py
@@ -6,10 +6,8 @@ from __future__ import absolute_import
 
 import os
 import re
-import collections
 import datetime
 import functools
-import itertools
 import uuid
 
 # Select a version of dbapi2 that's available.
@@ -192,7 +190,7 @@ class SQLiteConnection(object):
                 raise Error("Cannot load the spatialite extension (%s): %s" % (self._mod_spatialite, str(e)))
 
         # ensure that spatial metadata init has been done
-        with self:
+        with self._connection:
             cursor = self._connection.cursor()
             cursor.execute("PRAGMA table_info(geometry_columns);")
             if cursor.fetchall() == []:
@@ -203,7 +201,7 @@ class SQLiteConnection(object):
 
         # create the tables if necessary
         if need_prepare:
-            with self:
+            with self._connection:
                 sqls = self._backend._create_tables_sql()
                 self._backend._execute_list(sqls)
 