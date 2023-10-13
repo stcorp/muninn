65669480acbcb787dab9533049afefe43a56bfdd
Mark Dufour <mark.dufour@stcorp.nl>
2023-10-13 10:53:26 +0200

    test: add custom remote backend plugin

---
+++ test/myremoteext.py
@@ -0,0 +1,17 @@
+class MyRemoteExtension:
+    def identify(self, url):
+        return url.startswith('myremote:')
+
+    def pull(self, archive, product, target_dir):
+        assert False
+
+
+_remote_extensions = {
+    'myremote': MyRemoteExtension()
+}
+
+def remote_backends():
+    return _remote_extensions.keys()
+
+def remote_backend(name):
+    return _remote_extensions[name]
