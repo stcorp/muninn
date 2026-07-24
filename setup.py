bbe5a49a42b36536f344d143f65ca8313c66ef3d
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2026-07-24 10:49:40 +0200

    Create 7.4.0 release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = []
 
 setup(
     name="muninn",
-    version="7.3.0",
+    version="7.4.0",
     description="Data product catalogue and archive system",
     url="https://github.com/stcorp/muninn",
     author="S[&]T",
